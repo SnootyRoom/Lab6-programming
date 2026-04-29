d37d14c3a6657ff7c0db5072806bffd8a2a94338
SnootyRoom <126325976+SnootyRoom@users.noreply.github.com>
2026-04-29 20:39:00 +0700

    Class smart array created

---
+++ SmartArray/SmartArray.h
@@ -0,0 +1,20 @@
+#ifndef SMARTARRAY_H
+#define SMARTARRAY_H
+
+class SmartArray
+{
+    private:
+        unsigned int size;
+        int* data[];
+    
+    public:
+        SmartArray(unsigned int n);
+
+        ~SmartArray();
+
+        void setElement(int index, int value);
+
+        int getElemet();
+};
+
+#endif
